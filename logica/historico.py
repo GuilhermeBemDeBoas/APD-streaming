0587e28cc264db672fd7fffd254fe580f211c083
Cristiano Souza de Oliveira <cristiano.so@hotmail.com>
2017-10-25 10:04:14 -0200

    Começando implementação do modulo historico

---
+++ logica/historico.py
@@ -0,0 +1,12 @@
+from logica import filme
+from logica import usuario
+
+filmes_assistidos = []
+
+def registrar_filme_assistido(cod_filme,cpf):
+    filme = filme.buscar_filme(cod_filme)
+    pass
+
+def listar_filmes_assistidos(cpf):
+    pass
+
